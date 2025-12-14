a6bfb86f5e82de351a84461012f3ff6139a42aa4
Shorov Kumer Saha <shorovkumersaha@Shorovs-MacBook-Air.local>
2025-12-14 08:57:39 -0800

    Added donations route for creating blood donation requests inserting into MongoDB

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ async function run() {
         // Send a ping to confirm a successful connection
         const database = client.db("assignment11");
          const userCollection = database.collection("users");
+         const donationColections = database.collection("donation");
+
          app.post('/users',async(req,res)=>{
             const userInfo = req.body;
             userInfo.role = 'buyer';
@@ -41,6 +43,13 @@ async function run() {
             const query = {email:email};
             const result = await userCollection.findOne(query);
             res.send(result);
+        })
+
+        app.post('/donations', async(req,res)=>{
+            const data =req.body;
+            data.createdAt = new Date();
+            const result = await donationColections.insertOne(data)
+            res.send(result)
         })
 
         await client.db("admin").command({ ping: 1 });
